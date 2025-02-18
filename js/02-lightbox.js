d26421ac5057ad7094c872c6195532781d4163f2
nata20019 <nbernikova68@gmail.com>
2025-02-18 11:54:23 +0100

    mod7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,4 +3,23 @@ import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
-console.log(basicLightbox);
+const galleryList = document.querySelector(".gallery");
+const markupItems = galleryItems
+  .map(
+    (item) => `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+       alt="${item.description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+// console.log(markupItems);
+galleryList.insertAdjacentHTML("beforeend", markupItems);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
